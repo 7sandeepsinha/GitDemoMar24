543521d7e46af69e26e9ab330c577cd3a1630218
sndpoffcl <sandeep2official@gmail.com>
2024-03-30 21:47:28 +0530

    Added calculator

--- Main.java
+++ Main.java
@@ -1,5 +1,6 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World");
+        System.out.println("Addition " + Calculator.add(10,15));
     }
 }
