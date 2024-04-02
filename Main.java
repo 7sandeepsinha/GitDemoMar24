aa45b7e5f985c0d77b91f57886052876c8d5a08e
sndpoffcl <sandeep2official@gmail.com>
2024-04-02 21:51:50 +0530

    Added interest calculator

--- Main.java
+++ Main.java
@@ -2,6 +2,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World");
         System.out.println("Addition " + Calculator.add(10,15));
+        System.out.println("Interest " + InterestCalculator.calculate(100000, 8, 5));
     }
 }
 
